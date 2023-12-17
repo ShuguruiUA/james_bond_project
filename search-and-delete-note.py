809aead11de68837a9d7d7a5aa496125804390ae
SvitlanaMorenko <143277436+SvitlanaMorenko@users.noreply.github.com>
2023-12-17 13:07:19 +0100

    different search func

--- search-and-delete-note.py
+++ search-and-delete-note.py
@@ -2,16 +2,12 @@ from collections import UserDict
 
 class Notebook(UserDict):
    
-    def search(self, query):
-       results = []
-       for note in self.data.values():
-          if (
-            query.lower() in note.title.lower()
-            or any(query in teg.lower() for teg in note.tegs)
-            or query.lower() in note.note_body.lower()
-            ):
+    def search(self, query: str):
+        results = []
+        for note in self.data.values():
+            if query.lower() in note.note_body.lower():
                 results.append(note)
-       return results
+        return results
     
     def delete(self, title):
         self.pop(title, None)
